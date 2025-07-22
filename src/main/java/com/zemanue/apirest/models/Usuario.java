3cc09c360fec4037b3538baee95facc18888f369
zemanue <manureco.97@gmail.com>
2025-07-22 15:42:22 +0200

    Update entity model and configuration
    When running the application, the table is created in the database succesfully

--- src/main/java/com/zemanue/apirest/models/Usuario.java
+++ src/main/java/com/zemanue/apirest/models/Usuario.java
@@ -4,11 +4,14 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Table(name = "usuarios")
 @Data // Proporciona getters, setters, toString, etc. gracias a Lombok
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
     private Long id;
+    
     private String nombre;
     private String email;
 }
